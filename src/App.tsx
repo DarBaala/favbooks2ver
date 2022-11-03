4f716a2ef332a36d6dd8e2daede049e1e3fc1808
DarBaala <joker.milan1997@gmail.com>
2022-11-04 01:42:19 +0300

    Create Banner component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import "./scss/app.scss";
 
 import Header from "./components/Header";
 import Search from "./components/Search";
+import Banner from "./components/Banner";
+import Categories from "./components/Categories";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Search />
+      <Banner />
+      <Categories />
     </div>
   );
 };
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,45 @@
-import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, A11y, Autoplay } from "swiper";
+import { Link } from "react-router-dom";
+
+import "swiper/css";
+import "swiper/css/autoplay";
+import "swiper/css/pagination";
 
 const Banner = () => {
   return (
     <div className="container">
-      <div className="banner"></div>
+      <div className="banner">
+        <Swiper
+          modules={[Pagination, Autoplay, A11y]}
+          autoplay={{
+            delay: 5000,
+            pauseOnMouseEnter: true,
+            disableOnInteraction: false,
+          }}
+          spaceBetween={50}
+          slidesPerView={1}
+          pagination={{ clickable: true }}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>
+            <Link className="banner__photo" to="/banners">
+              <img src="img/banner.png" alt="Banner: Books" />
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/banners">
+              <img src="img/banner.png" alt="Banner: Books" />
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Link to="/banners">
+              <img src="img/banner.png" alt="Banner: Books" />
+            </Link>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 };
